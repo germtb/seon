3664528246e00f4fcd08f35d33dd8c06dfb45362
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2017-10-29 16:31:48 +0000

    split core libraries

--- bin/miniscript.js
+++ bin/miniscript.js
@@ -3,6 +3,7 @@
 const argv = require('yargs').argv
 const fs = require('fs')
 const path = require('path')
+const msCore = require('./ms-core')
 const run = require('../dist/interpreter').run
 
 const pwd = process.env.PWD
@@ -10,64 +11,4 @@ const filename = path.resolve(pwd, argv._[0])
 const dirname = path.dirname(filename)
 const file = fs.readFileSync(filename, 'utf8')
 
-const toJSString = (x, tabulation = '') => {
-	if (x.type === 'NumberExpression') {
-		return x.value
-	} else if (x.type === 'BooleanExpression') {
-		return x.value ? 'true' : 'false'
-	} else if (x.type === 'StringExpression') {
-		return x.value
-	} else if (x.type === 'FunctionExpression') {
-		return 'FunctionExpression'
-	} else if (x.type === 'ObjectExpression') {
-		if (x.properties.length === 0) {
-			return '{}'
-		} else if (x.properties.length === 1) {
-			return '{ ' + toJSString(x.properties[0]) + ' }'
-		}
-
-		return (
-			'{\n' +
-			x.properties.map(p => toJSString(p, tabulation + '  ')).join('\n') +
-			'\n}'
-		)
-	} else if (x.type === 'ObjectProperty') {
-		return (
-			tabulation +
-			x.property.name +
-			': ' +
-			toJSString(x.property.value, tabulation)
-		)
-	} else if (x.type === 'ArrayExpression') {
-		return '[' + x.values.map(toJSString).join(', ') + ']'
-	}
-
-	return ''
-}
-
-const toString = {
-	type: 'Function',
-	call: params => ({
-		type: 'String',
-		value: params.map(p => toJSString(p)).join(' ')
-	})
-}
-
-const log = {
-	type: 'Function',
-	call: params => {
-		const logResult = params.map(p => toJSString(p)).join(' ')
-		// eslint-disable-next-line
-		console.log(logResult)
-	}
-}
-
-const type = {
-	type: 'Function',
-	call: params => ({
-		type: 'String',
-		value: params[0].type
-	})
-}
-
-run(file, [{ filename, dirname, log, toString, type }])
+run(file, [{ filename, dirname, ...msCore }])
---
+++ bin/ms-core.js
@@ -0,0 +1,59 @@
+const toJSString = (x, tabulation = '') => {
+	if (x.type === 'NumberExpression') {
+		return x.value
+	} else if (x.type === 'BooleanExpression') {
+		return x.value ? 'true' : 'false'
+	} else if (x.type === 'StringExpression') {
+		return x.value
+	} else if (x.type === 'FunctionExpression') {
+		return 'FunctionExpression'
+	} else if (x.type === 'ObjectExpression') {
+		if (x.properties.length === 0) {
+			return '{}'
+		} else if (x.properties.length === 1) {
+			return '{ ' + toJSString(x.properties[0]) + ' }'
+		}
+
+		return (
+			'{\n' +
+			x.properties.map(p => toJSString(p, tabulation + '  ')).join('\n') +
+			'\n}'
+		)
+	} else if (x.type === 'ObjectProperty') {
+		return (
+			tabulation +
+			x.property.name +
+			': ' +
+			toJSString(x.property.value, tabulation)
+		)
+	} else if (x.type === 'ArrayExpression') {
+		return '[' + x.values.map(toJSString).join(', ') + ']'
+	}
+
+	return ''
+}
+
+export const toString = {
+	type: 'Function',
+	call: params => ({
+		type: 'String',
+		value: params.map(p => toJSString(p)).join(' ')
+	})
+}
+
+export const log = {
+	type: 'Function',
+	call: params => {
+		const logResult = params.map(p => toJSString(p)).join(' ')
+		// eslint-disable-next-line
+		console.log(logResult)
+	}
+}
+
+export const type = {
+	type: 'Function',
+	call: params => ({
+		type: 'String',
+		value: params[0].type
+	})
+}
